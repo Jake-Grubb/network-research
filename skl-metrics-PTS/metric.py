384f787468529963dacfc447e6789ca7a7747f6f
Jake Grubb <jgrubb013@gmail.com>
2019-06-26 14:55:38 -0500

    added more files

--- skl-metrics-PTS/metric.py
+++ skl-metrics-PTS/metric.py
@@ -10,5 +10,9 @@ import sys
 
 def main():
 	if(len(sys.argv) != 3):
-		print("Error: invalid syntax\n Correct Syntax: python3 metric.py <input file> <metric-type>\nValid metric types: bouldin, silhouette")
+		print("Error: invalid syntax\nCorrect Syntax: python3 metric.py <input file> <metric-type>\nValid metric types: bouldin, silhouette")
 		sys.exit(0)
+	pfile = pandas.read_csv(sys.argv[1])
+
+
+main()
