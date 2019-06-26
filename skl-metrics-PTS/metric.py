8d3ef9caa64f1c17078fb1b907e86a63302cf042
Jake Grubb <jgrubb013@gmail.com>
2019-06-26 14:24:24 -0500

    Adding basic structure to scripts

--- skl-metrics-PTS/metric.py
+++ skl-metrics-PTS/metric.py
@@ -1,6 +1,14 @@
 #Programmer: Jacob Grubb
 #Organization: SIUE CS Department
 #Project: scikit-learn clustering metrics
-#File: metric.py 
+#File: metric.py
 
 import sklearn
+import pandas
+import sys
+
+
+def main():
+	if(len(sys.argv) != 3):
+		print("Error: invalid syntax\n Correct Syntax: python3 metric.py <input file> <metric-type>\nValid metric types: bouldin, silhouette")
+		sys.exit(0)
