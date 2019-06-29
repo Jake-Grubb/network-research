bb8d2127766a7fe37492da72d551c79f1e3c0185
Jake Grubb <jgrubb013@gmail.com>
2019-06-29 14:59:05 -0500

    Implemented functions fully

--- skl-metrics-PTS/metric.py
+++ skl-metrics-PTS/metric.py
@@ -7,26 +7,33 @@ import sklearn.metrics as skm
 import pandas
 import sys
 
-
 def main():
-	if(len(sys.argv) != 3):
-		print("Error: invalid syntax\nCorrect Syntax: python3 metric.py <input file> <metric-type>\nValid metric types: boul, silh")
+	if(len(sys.argv) != 4):
+		print("Error: invalid syntax\nCorrect Syntax: python3 metric.py <input file> <metric-type> <target column>\nValid metric types: boul, silh")
 		sys.exit(0)
 	print("Reading file...")
 	pfile = pandas.read_csv(sys.argv[1])
+	pfile = pfile.drop(pfile.index[0])
 	print("Success")
 	mode = sys.argv[2]
+	target_column = sys.argv[3]
 	if(mode != 'boul' and mode != 'silh'):
 		print("Error: invalid syntax\nCorrect Syntax: python3 metric.py <input file> <metric-type>\nValid metric types: boul, silh")
 		sys.exit(0)
 	if(mode == 'boul'):
-		runBoul(pfile)
+		value = runBoul(pfile, target_column)
 	elif(mode == 'silh'):
-		runSilh(pfile)
+		value = runSilh(pfile, target_column)
+	with open("./" + sys.argv[1] + "_" + mode + "_results.txt", "w") as outFile:
+		outFile.write("mode " + "value: " + str(value))
 	return
 
-def runBoul(pfile):
-	skm.davies_bouldin_score(pfile.values, pfile.columns)
-def runSilh(pfile):
-	skm.silhouette_score(pfile.values, pfile.columns)
+def runBoul(pfile, results_name):
+	results_column = pfile[results_name]
+	pfile.drop(results_name, axis=1, inplace = True)
+	return skm.davies_bouldin_score(pfile, results_column)
+def runSilh(pfile, results_name):
+	results_column = pfile[results_name]
+	pfile.drop(results_name, axis=1, inplace = True)
+	return skm.silhouette_score(pfile, results_column)
 main()
