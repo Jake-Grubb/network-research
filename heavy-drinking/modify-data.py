24016a5346700e9da77c8e7974109c9b3c62e60f
Jake-Grubb <jgrubb013@gmail.com>
2019-08-26 16:41:47 +0000

    Added experiment and python shell scripts

---
+++ heavy-drinking/modify-data.py
@@ -0,0 +1,5 @@
+# Project: heavy-drinking
+# Programmer: Jacob Grubb
+# Organization: SIUE CS Department
+# File: modify-data.py
+# Description: Python script for modifying the existing REGARDS dataset, adding new features for testing against.
